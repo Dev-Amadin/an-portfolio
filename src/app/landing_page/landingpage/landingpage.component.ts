fdc572a6e081d023927da71de8e1c6cf4ad3ff0a
Amadin Nosa-Idemudia <anosaidemudia@gmail.com>
2025-09-30 17:48:25 +0100

    about section addition

--- src/app/landing_page/landingpage/landingpage.component.ts
+++ src/app/landing_page/landingpage/landingpage.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../navbar/navbar.component";
 import { HeroComponent } from '../../hero/hero.component';
+import { AboutComponent } from "../../about/about.component";
 
 @Component({
   selector: 'app-landingpage',
   standalone: true,
-  imports: [NavbarComponent, HeroComponent],
+  imports: [NavbarComponent, HeroComponent, AboutComponent],
   templateUrl: './landingpage.component.html',
   styleUrl: './landingpage.component.scss'
 })
