0fd02f99e3e89f05385170f5b17e4c10adf08b0e
Amadin Nosa-Idemudia <anosaidemudia@gmail.com>
2025-10-01 18:19:57 +0100

    skills addition

--- src/app/landing_page/landingpage/landingpage.component.ts
+++ src/app/landing_page/landingpage/landingpage.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from "../../navbar/navbar.component";
 import { HeroComponent } from '../../hero/hero.component';
 import { AboutComponent } from "../../about/about.component";
+import { SkillsComponent } from "../../skills/skills.component";
 
 @Component({
   selector: 'app-landingpage',
   standalone: true,
-  imports: [NavbarComponent, HeroComponent, AboutComponent],
+  imports: [NavbarComponent, HeroComponent, AboutComponent, SkillsComponent],
   templateUrl: './landingpage.component.html',
   styleUrl: './landingpage.component.scss'
 })
---
+++ src/app/skills/skills.component.ts
@@ -0,0 +1,62 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'skills',
+  standalone: true,
+  imports: [],
+  templateUrl: './skills.component.html',
+  styleUrl: './skills.component.scss',
+})
+export class SkillsComponent {
+  skillData = [
+    {
+      title: 'Frontend',
+      skills: [
+        { name: 'HTML5', icon: '../../assets/logos/html5.svg' },
+        { name: 'CSS3', icon: '../../assets/logos/css3.svg' },
+        { name: 'SCSS', icon: '../../assets/logos/sass.svg' },
+        { name: 'Javascript', icon: '../../assets/logos/file-code.svg' },
+        { name: 'TypeScript', icon: '../../assets/logos/file-code.svg' },
+        { name: 'Angular', icon: '../../assets/logos/angular.svg' },
+        { name: 'Tailwind', icon: '../../assets/logos/file-code.svg' },
+        { name: 'RxJS', icon: '../../assets/logos/file-code.svg' },
+        { name: 'Bootstrap', icon: '../../assets/logos/bootstrap.svg' },
+      ],
+    },
+    {
+      title: 'Backend',
+      skills: [
+        { name: 'Spring Boot', icon: '../../assets/logos/file-code.svg' },
+        { name: 'MySQL', icon: '../../assets/logos/file-code.svg' },
+        { name: 'ProgreSQL', icon: '../../assets/logos/file-code.svg' },
+        { name: 'OracleDB', icon: '../../assets/logos/file-code.svg' },
+        { name: 'RESTful APIs', icon: '../../assets/logos/file-code.svg' },
+        { name: 'Firebase', icon: '../../assets/logos/file-code.svg' },
+      ],
+    },
+    {
+      title: 'Mobile',
+      skills: [{ name: 'Ionic', icon: '../../assets/logos/file-code.svg' }],
+    },
+    {
+      title: 'Languages',
+      skills: [
+        { name: 'JavaScript', icon: '../../assets/logos/file-code.svg' },
+        { name: 'TypeScript', icon: '../../assets/logos/file-code.svg' },
+         { name: 'Java', icon: '../../assets/logos/java.svg' },
+      ],
+    },
+    {
+      title: 'Tools',
+      skills: [
+        { name: 'Git', icon: '../../assets/logos/git.svg' },
+        { name: 'GitHub', icon: '../../assets/logos/github.svg' },
+        { name: 'GitLab', icon: '../../assets/logos/gitlab.svg' },
+        { name: 'Bitbucket', icon: '../../assets/logos/bitbucket.svg' },
+        { name: 'Postman', icon: '../../assets/logos/file-code.svg' },
+        { name: 'VS code', icon: '../../assets/logos/file-code.svg' },
+        { name: 'InteliJ', icon: '../../assets/logos/file-code.svg' },
+      ],
+    },
+  ];
+}
