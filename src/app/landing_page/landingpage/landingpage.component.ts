2c231577b576ce7318c0c39d4284ccfc22dae5b8
Amadin Nosa-Idemudia <anosaidemudia@gmail.com>
2025-10-02 16:00:55 +0100

    contact addition

--- src/app/landing_page/landingpage/landingpage.component.ts
+++ src/app/landing_page/landingpage/landingpage.component.ts
@@ -5,11 +5,12 @@ import { AboutComponent } from "../../about/about.component";
 import { SkillsComponent } from "../../skills/skills.component";
 import { ExperienceComponent } from "../../experience/experience.component";
 import { ProjectsComponent } from "../../projects/projects.component";
+import { ContactComponent } from "../../contact/contact.component";
 
 @Component({
   selector: 'app-landingpage',
   standalone: true,
-  imports: [NavbarComponent, HeroComponent, AboutComponent, SkillsComponent, ExperienceComponent, ProjectsComponent],
+  imports: [NavbarComponent, HeroComponent, AboutComponent, SkillsComponent, ExperienceComponent, ProjectsComponent, ContactComponent],
   templateUrl: './landingpage.component.html',
   styleUrl: './landingpage.component.scss'
 })
